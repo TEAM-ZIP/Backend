c2eacfdd7d3f404b442d476f016aed7790fd8b31
topograp2 <topo.or.dubu@gmail.com>
2025-01-22 15:01:38 +0900

    Feat(#1): 카카오로그인

--- src/main/java/com/capstone/bszip/dto/LoginResponse.java
+++ src/main/java/com/capstone/bszip/dto/LoginResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 public class LoginResponse {
     private Long id;
     private String nickname;
-    private String email;;
+    private String email;
     private AuthTokens token;
 
     public LoginResponse(Long id, String nickname, String email, AuthTokens token){
